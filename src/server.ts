db9da25fceaaa976f465a7ae684849a8c60b7c81
Yauheni_Tsurhanau <Yauheni_Tsurhanau@epam.com>
2023-04-08 22:31:21 +0400

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { Express } from "express";
+import { router } from "./api/routes/userRoutes";
+
+const app: Express = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/users", router);
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log("Server is listening on PORT: 3000");
+});
