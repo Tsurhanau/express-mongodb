0f4b9d00277a5b010e83b44fde5efbab76414c1f
Yauheni_Tsurhanau <Yauheni_Tsurhanau@epam.com>
2023-04-09 16:55:17 +0400

    feat: add user crud mongo

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,7 +1,11 @@
-export type User = {
-    id: string;
-    login: string;
-    password: string;
-    age: number;
-    isDeleted: boolean;
-  };
+const mongoose = require("mongoose");
+const Schema  = mongoose.Schema;
+
+const UserSchema = new Schema({
+  login: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  age: { type: Number, required: true },
+  isDeleted: { type: Boolean, required: false }
+});
+
+export const UserModel = mongoose.model("User", UserSchema);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import express from "express";
 import { Express } from "express";
 import { router } from "./api/routes/userRoutes";
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+mongoose
+.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => { console.log("Connected to MongoDB") })
+.catch((err: any) => { console.log(err) });
 
 const app: Express = express();
 
@@ -11,6 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use("/users", router);
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log("Server is listening on PORT: 3000");
+app.listen(process.env.PORT, () => {
+	console.log(`Server is listening on PORT: ${process.env.PORT}`);
 });
